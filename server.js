557cb66b438cc7c354f65486ad5bab5a5dec539e
lojeremy <lokwanlokjeremy@gmail.com>
2015-05-08 01:06:23 +0800

    a

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 var express = require('express');
 var app = express();
 
-// app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
 app.use( express.static( __dirname + '/public' ) );
 
+/*
 var server_port = process.env.PORT || 8000;
 var server_ip_address = process.env.IP || '127.0.0.1';
+*/
 
 app.get( '/', function ( request, response ) {
 	// response.send('Hello World!');
 	response.sendFile( __dirname + '/views/index.html' );
 } );
 
+/*
 var server = app.listen( server_port, server_ip_address, function () {
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log( 'Listening at http://%s:%s', host, port );
 } );
+*/
 
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
